3b9b3c557dbc71b6edbcce5458f818967e18e013
Sofja <113921929+SofjaV@users.noreply.github.com>
2022-10-06 17:01:24 +0300

    Add files via upload

---
+++ homework_07.py
@@ -0,0 +1,29 @@
+# Написать программу которая открывает текстовый файл и считает следующее:
+# 1. Общее кол-во слов
+# 2. Кол-во уникальных (разных)
+#
+# Не влияет на уникальность:
+# Заглавные и прописные буквы
+# Знаки препинания: ',' '.' '!' '?'
+#
+# Сохраняет кол-ва в отдельный файл.
+# Выписывает все уникальные слова в алфавитном порядке (по одному слову в строке).
+
+
+with open(f'tri.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
+    data = data.lower()
+    data = data.replace('\n', ' ').replace(',', ' ').replace('.', ' ').replace('!', ' ').replace('?', ' ').replace('"', ' ').replace(' - ', ' ').replace(':', ' ').replace(';', ' ').replace('*', ' ')
+    words = data.split(' ')
+
+words_unique = []
+for word in words:
+    if word not in words_unique:
+        words_unique.append(word)
+words_unique.sort()
+print('\n'.join(words_unique))
+
+with open(f'new_file.txt', 'w', encoding='utf-8') as new_file:
+    new_file.write(str((len(words_unique))))
+    new_file.write('\n')
+    new_file.write(str((len(words))))
